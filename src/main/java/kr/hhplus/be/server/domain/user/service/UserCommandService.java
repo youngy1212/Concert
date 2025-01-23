654e7fa7794c6ac577a2b5e3c051dd617677e761
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-24 03:38:33 +0900

    feat : 잔액 충전 동시성 처리 (낙관적 락)

--- src/main/java/kr/hhplus/be/server/domain/user/service/UserCommandService.java
+++ src/main/java/kr/hhplus/be/server/domain/user/service/UserCommandService.java
@@ -1,6 +1,8 @@
 package kr.hhplus.be.server.domain.user.service;
 
+import jakarta.persistence.OptimisticLockException;
 import java.util.NoSuchElementException;
+import kr.hhplus.be.server.domain.common.exception.CustomException;
 import kr.hhplus.be.server.domain.user.model.Point;
 import kr.hhplus.be.server.domain.user.repository.UserCommand;
 import kr.hhplus.be.server.domain.user.service.dto.ChargeDto;
@@ -17,10 +19,16 @@ public class UserCommandService {
     @Transactional
     public ChargeDto chargePoint(Long userId, Long amount) {
 
+        try {
+
         Point point = userCommand.charge(userId).orElseThrow(() -> new NoSuchElementException("포인트를 찾을 수 없습니다."));
         point.charge(amount);
 
         return new ChargeDto(point.getUser().getId(), point.getAmount());
+        } catch (OptimisticLockException e) {
+            // 낙관적 락 충돌 처리
+            throw new CustomException("포인트 충전 중 충돌이 발생했습니다. 다시 시도해주세요.");
+        }
 
     }
 }
