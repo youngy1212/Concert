c12f7c4f6b04b9ed030379f68fd3811e43d3079a
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-02-02 08:04:12 +0900

    refactor : 검증 책임 분리 (reservation)

--- src/main/java/kr/hhplus/be/server/application/ReservationFacade.java
+++ src/main/java/kr/hhplus/be/server/application/ReservationFacade.java
@@ -1,15 +1,12 @@
 package kr.hhplus.be.server.application;
 
 
-import java.util.List;
 import kr.hhplus.be.server.annotation.DistributedLock;
 import kr.hhplus.be.server.application.dto.ReservationDto;
-import kr.hhplus.be.server.domain.common.exception.CustomException;
 import kr.hhplus.be.server.domain.concert.model.ConcertSchedule;
 import kr.hhplus.be.server.domain.concert.model.Seat;
 import kr.hhplus.be.server.domain.concert.service.ConcertQueryService;
 import kr.hhplus.be.server.domain.reservation.model.Reservation;
-import kr.hhplus.be.server.domain.reservation.model.ReservationStatus;
 import kr.hhplus.be.server.domain.reservation.service.ReservationCommandService;
 import kr.hhplus.be.server.domain.reservation.service.ReservationQueryService;
 import kr.hhplus.be.server.domain.user.model.User;
@@ -37,14 +34,7 @@ public class ReservationFacade {
         ConcertSchedule concertSchedule = concertQueryService.getConcertSchedule(concertScheduleId);
         Seat seat = concertQueryService.getSeat(seatId);
 
-        List<ReservationStatus> statuses = List.of(ReservationStatus.RESERVED, ReservationStatus.BOOKED);
-        boolean existing = reservationQueryService.existingReservation(concertScheduleId,seatId,statuses );
-
-
-        if (existing) {
-            throw new CustomException("이미 선택된 좌석입니다.");
-        }
-
+        reservationQueryService.existingReservation(concertScheduleId,seatId);
         Reservation reservation = reservationCommandService.createReservation(concertSchedule, user,
                 seat, tokenId);
 
--- src/main/java/kr/hhplus/be/server/domain/reservation/service/ReservationQueryService.java
+++ src/main/java/kr/hhplus/be/server/domain/reservation/service/ReservationQueryService.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.domain.reservation.service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import kr.hhplus.be.server.domain.common.exception.CustomException;
 import kr.hhplus.be.server.domain.reservation.model.Reservation;
 import kr.hhplus.be.server.domain.reservation.model.ReservationStatus;
 import kr.hhplus.be.server.domain.reservation.repository.ReservationQuery;
@@ -20,7 +21,13 @@ public class ReservationQueryService {
 
     }
 
-    public boolean existingReservation(final Long concertScheduleId, final Long seatId, List<ReservationStatus> statuses) {
-        return reservationQuery.existingReservation(concertScheduleId,seatId,statuses );
+    public void existingReservation(final Long concertScheduleId, final Long seatId) {
+
+        List<ReservationStatus> statuses = List.of(ReservationStatus.RESERVED, ReservationStatus.BOOKED);
+        boolean existing = reservationQuery.existingReservation(concertScheduleId,seatId,statuses);
+
+        if (existing) {
+            throw new CustomException("이미 선택된 좌석입니다.");
+        }
     }
 }
