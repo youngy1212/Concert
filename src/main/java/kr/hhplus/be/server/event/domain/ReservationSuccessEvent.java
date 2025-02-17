75a39ba4af1f9d049c8625f215e3135fee74a3f8
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-02-17 21:57:37 +0900

    init : event 객체 추가

--- src/main/java/kr/hhplus/be/server/event/domain/ReservationSuccessEvent.java
+++ src/main/java/kr/hhplus/be/server/event/domain/ReservationSuccessEvent.java
@@ -1,10 +1,17 @@
 package kr.hhplus.be.server.event.domain;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 public class ReservationSuccessEvent {
 
+    private LocalDateTime occurredAt;  // 이벤트 발생 시간
+    private String eventId;            // 이벤트 고유 식별자
+    private Map<String, Object> metadata; // 추가 메타데이터
     private Long reservationId;
+
 
     public ReservationSuccessEvent(Long id) {
     }
-    //이벤트 내부 객체에 뭐가 들어가야할까..?
+
 }
