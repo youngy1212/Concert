9f708e1ae452faf1163fb4bfaf73db5a1594a03b
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-02-21 06:16:16 +0900

    feat : 이벤트 kafka 로 변경

--- src/main/java/kr/hhplus/be/server/event/ReservationEventPublisher.java
+++
@@ -1,7 +0,0 @@
-package kr.hhplus.be.server.event;
-
-import kr.hhplus.be.server.event.domain.ReservationSuccessEvent;
-
-public interface ReservationEventPublisher {
-    void send(ReservationSuccessEvent event);
-}
